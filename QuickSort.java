ade0e9d5ec75f5e69b2e220c58db2be55fdb5792
gauravbhandari07 <78907564+gauravbhandari07@users.noreply.github.com>
2023-01-08 22:16:39 +0530

    Add files via upload

---
+++ QuickSort.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class QuickSort
+{
+    public static int partition(int A[], int l, int u)
+    {
+        int pivot= A[u];
+        int i=l-1;
+         
+        for(int j=l;j<u; j++)
+        {
+            if(A[j]<pivot)
+            {
+                i++;
+                int temp=A[i];
+                A[i]=A[j];
+                A[j]=temp;
+            }
+        }
+        i++;
+        int temp=A[i];
+        A[i]=pivot;
+        A[u]=temp;
+        return i;
+    }
+    public static void quickSort(int A[], int l, int u)
+    {
+        if(l<u)
+        {
+            int pind= partition(A, l, u);
+            quickSort(A, l, pind-1);
+            quickSort(A, pind+1, u);
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int a, n;
+        int A[]= new int[100];
+        System.out.println("enter the size of array: ");
+        a=sc.nextInt();
+        System.out.println("enter the array elements:");
+        for(int i=0;i<a;i++)
+        {
+            A[i]=sc.nextInt();
+        }
+        quickSort(A,0, a-1);
+        System.out.println("Array after sorting");
+        for(int i=0;i<a;i++)
+        {
+            System.out.println(A[i]);
+        }
+    }
+}
+
+ 
