5721f0ec544ef869d90bec72e0fac53f90a3059a
gauravbhandari07 <78907564+gauravbhandari07@users.noreply.github.com>
2023-01-14 20:33:19 +0530

    Update HeapSort.java

--- HeapSort.java
+++ HeapSort.java
@@ -35,14 +35,6 @@ public class HeapSort {
             heapify(arr, N, largest);
         }
     }
-    static void printArray(int arr[])
-    {
-        int N = arr.length;
-  
-        for (int i = 0; i < N; ++i)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
     public static void main(String args[])
     {
         int arr[] = { 12, 11, 13, 5, 6, 7 };
@@ -52,6 +44,8 @@ public class HeapSort {
         ob.sort(arr);
   
         System.out.println("Sorted array is");
-        printArray(arr);
+        for (int i = 0; i < N; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
     }
 }
